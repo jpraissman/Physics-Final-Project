8a1be98c1bfa47ff40b081d28e9a1ea0670b8bed
jpraissman <jordanpraissman@gmail.com>
2023-06-09 13:17:04 -0400

    Initial Commit

---
+++ finalProject/finalProject/GameRunner.java
@@ -0,0 +1,27 @@
+package finalProject;
+
+import processing.core.PApplet;
+
+public class GameRunner extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("finalProject.GameRunner");
+
+	}
+	
+	public void settings()
+    {
+    	size(700, 700);
+    	
+    }
+    
+    public void setup()
+    {
+    	
+    }
+    
+    public void draw() {
+    	text("Test", 400, 50);
+    }
+
+}
