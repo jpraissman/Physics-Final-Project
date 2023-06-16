9c913b8bc258cc27b21afe06ecf1bf0c48fca6b8
aFerreira27 <90414431+aFerreira27@users.noreply.github.com>
2023-06-15 21:53:51 -0400

    idk

---
+++ finalProject/finalProject/Home.java
@@ -0,0 +1,63 @@
+package finalProject;
+import processing.core.PApplet;
+import processing.core.PImage;
+//import processing.net.Client;
+
+public class Home extends PApplet
+{
+	PImage background;
+	
+	public static void main(String[] args)
+	{
+		PApplet.main("finalProject.Home");
+
+	}
+	
+	public void settings() {
+		size(1000, 750);
+	}
+	
+	public void setup()
+	{
+		background = loadImage("assets/Home.png");
+		image(background, 0, 0);
+	}
+	
+	public void draw()
+	{
+		
+	}
+	
+	public void mouseClicked()
+	{
+		int col = get(mouseX, mouseY);
+		System.out.println(col + ": " + mouseX +", " + mouseY);
+		if((col == -1 || col == -16711936))
+		{
+			if(mouseX >= 330 && mouseX <= 660 && mouseY >= 260 && mouseY <= 485)
+			{
+				if(mouseY < 375)
+				{
+					System.out.println("singlePlayer");
+					
+				}
+				else
+				{
+					System.out.println("multiplayer");
+				}
+			}
+			else if (mouseY >= 490 && mouseY <= 600 && mouseX >= 155 && mouseX <= 840)
+			{
+				if(mouseX <= 500)
+				{
+					System.out.println("tutorial");
+				}
+				else
+				{
+					System.out.println("song");
+				}		
+			}
+		}
+
+	}
+}
